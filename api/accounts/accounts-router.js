5a0b0b2f0328497d46c4137f2aabea4721b2a2bc
Jessica <duellje@oregonstate.edu>
2021-02-25 18:19:25 -0500

    router get / route working

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,31 +1,50 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const accounts_model = require("./accounts-model");
+const {
+  checkAccountId,
+  checkAccountNameUnique,
+  checkAccountPayload,
+} = require("./accounts-middleware");
 
-router.get('/', async (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get("/", async (req, res, next) => {
+  accounts_model
+    .getAll()
+    .then((accounts) => {
+      res.status(200).json(accounts);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({
+          error: err,
+          message: "Error in retrieving accounts from database",
+        });
+    });
+});
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   // DO YOUR MAGIC
 });
 
-router.delete('/:id', (req, res, next) => {
+router.post("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.put("/:id", (req, res, next) => {
+  // DO YOUR MAGIC
+});
+
+router.delete("/:id", (req, res, next) => {
+  // DO YOUR MAGIC
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
   // CALL next(err) IF THE PROMISE REJECTS INSIDE YOUR ENDPOINTS
   res.status(500).json({
-    message: 'something went wrong inside the accounts router',
+    message: "something went wrong inside the accounts router",
     errMessage: err.message,
-  })
-})
+  });
+});
 
 module.exports = router;
